1437f078d9a925363e9f878496fa44bdfeeb7a81
Saimon8420 <latifulkabir567@gmail.com>
2023-10-31 18:29:58 +0600

    live link added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# FlowerShop [Live site:]()
+# FlowerShop [goto live site](https://6540f2855855a267b8de6c99--lovely-alpaca-01ecd9.netlify.app/)
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,9 @@ const Banner = () => {
     return (
         <div className='displayBanner'>
             <div className='bannerInfo'>
-                <h1>Let's make beautiful flowers a part of your life.</h1>
+                <div className="headerDiv">
+                    <h1 className="text">Let's make beautiful flowers a part of your life.</h1>
+                </div>
                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
                 <button>Shop Now</button>
             </div>
